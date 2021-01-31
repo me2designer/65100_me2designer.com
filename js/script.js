b300a74ced73da77b95a1c5771cda4953bd99d59
designer <me2designer.com>
2021-02-01 00:30:04 +0900

    test

--- js/script.js
+++ js/script.js
@@ -17,18 +17,74 @@ $(function(){ // DOCUMENT READY...
 */(function(){
 
 
-    
+
+    $.fn.aaaa = function(){
+        var $this = $(this);
+        var arg = arguments[0];
+
+        console.log('a');
+
+        if (arg.afterLoad){
+            arg.afterLoad();
+        }
+    }
+
+    $('h5').aaaa({
+        // afterLoad : function(){
+        //     console.log('b');
+        // }
+    });
 
 
 
 })();/*
 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 */(function(){
-    
-    
-  
-    
 
+
+
+    return;
+
+    function LAYER () {
+        var arg = arguments[0];
+        var $target = $(arg.target);
+        
+    
+        if (arg.afterLoad){
+            arg.afterLoad();
+        }
+
+    }
+
+    LAYER({
+        target : 'h5',
+        afterLoad : function(){
+            console.log('b');
+        }
+    });
+
+
+
+
+
+})();/*
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+*/(function(){
+
+
+    return;
+
+    function aaa(callback){
+        console.log('1');
+        callback();
+    }
+
+
+    aaa(function(){
+        console.log('2');
+        console.log('3');
+        console.log('4');
+    });
 
 
 })();/*
