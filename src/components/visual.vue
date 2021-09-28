d57f88807c07e972a69ffbb82770f5e410b70830
me2designer.com <me2designer@naver.com>
2021-09-28 13:35:30 +0900

    지역 컴포넌트 추가

---
+++ src/components/visual.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>메인</p>
+</template>
+
+<script>
+export default {
+    name: 'visual'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.vue
@@ -0,0 +1,17 @@
+<template>
+    <visual/>
+</template>
+
+<script>
+import visual from './components/visual.vue';
+
+export default {
+    components: {
+        visual
+    }
+}
+</script>
+
+<style>
+
+</style>
