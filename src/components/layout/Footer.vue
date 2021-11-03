2e0b96333e17fb2d246c22f4261a7bef5dd6b145
designer <me2designer.com>
2021-11-04 00:55:14 +0900

    footer 추가

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,15 +1,40 @@
 <template>
-    <div id="footer">
-        <p>footer.vue</p>
+<!-- #footer -->
+<div id="footer" role="contentinfo">
+    <div class="left">
+        <h2 class="tit">ME2DESIGNER</h2>
+        <p class="desc">궁금하신 내용은 이메일 또는 카카오톡 오픈채팅으로 문의해주세요</p>
+        <div class="list_sns">
+            <a class="link_sns" href="mailto:me2designer@naver.com" target="_blank" title="메일"><img class="mail" src="" data-images-path="/images/sns/mail02.svg"></a>
+            <a class="link_sns" href="https://open.kakao.com/o/sgydGNHd" target="_blank" title="카카오톡 오픈채팅"><img class="kakao" src="" data-images-path="/images/sns/kakaotalk02.svg"></a>
+            <a class="link_sns" href="https://blog.naver.com/hymt18" target="_blank" title="네이버 블로그"><img class="blog" src="" data-images-path="/images/sns/blog02.svg"></a>
+            <a class="link_sns" href="https://www.instagram.com/hymt18" target="_blank" title="인스타그램"><img class="instagram" src="" data-images-path="/images/sns/facebook02.svg"></a>
+            <a class="link_sns" href="https://www.facebook.com/me2designer" target="_blank" title="인스타그램"><img class="instagram" src="" data-images-path="/images/sns/instagram02.svg"></a>
+            <a class="link_sns" href="https://github.com/me2designer" target="_blank" title="GitHub"><img class="github" src="" data-images-path="/images/sns/github02.svg"></a>
+        </div>
     </div>
+    <div class="right">
+        <p class="tit">가장 쓸모 있는<br>경험을 제공하기 위해<br>UI를 개발 합니다</p>
+        <p class="desc">ME2DESIGNER.COM All rights reserved </p>
+    </div>
+</div>
+<!-- //#footer -->
 </template>
+
+
 
 <script>
 export default {
-
+    data() {
+        return {
+            imgPath: '/img/common/layout/'
+        }
+    }
 }
 </script>
 
-<style>
 
+
+<style lang="scss">
+    @import '@/assets/scss/common/layout.scss';
 </style>
--- src/views/main.vue
+++ src/views/main.vue
@@ -1,163 +1,165 @@
 <template>
-    <div id="container">
-        <!-- 메인비주얼 -->
-        <section id="visual">
-            <div class="inner">
-                <h3 class="tit_section">안녕하세요,<br><strong>UI 마크업 개발자 장지훈</strong> 입니다.</h3>
-                <button class="videoLayer" data-src="https://www.youtube.com/embed/sqgxcCjD04s" data-title="영상제목, 말줄임 오류있음" data-type="youtube" type="button" title="영상재생">1분 자기소개 영상</button>
-            </div> 
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide" :style="'background-image:url('+imgPath+'visual_bg1.jpg)'">
-                        <div class="inner">
-                            <p class="tit">다양한 디바이스 크로스 부라우징 기술력을 확보한<br>UI개발을 지원하고있습니다.</p>
-                        </div>
-                    </div>
-                    <div class="swiper-slide" style="background-image:url(https://via.placeholder.com/1920x900/89c997)">
-                        <div class="inner">
-                            <p class="tit">생산성으로<br>가장 효율적인 방법으로</p>
-                        </div>
-                    </div>
-                    <div class="swiper-slide" style="background-image:url(https://via.placeholder.com/1920x900/7ecef4)">
-                        <div class="inner">
-                            <p class="tit">광고 디자인~개발까지<br>다양한 역량 보유</p>
-                        </div>
-                    </div>
-                    <div class="swiper-slide" style="background-image:url(https://via.placeholder.com/1920x900/f29b76)">
-                        <div class="inner">
-                            <p class="tit">SEO, 시멘트 마크업으로<br>트래픽 향상에 최적</p>
-                        </div>
-                    </div>
-                    <div class="swiper-slide" style="background-image:url(https://via.placeholder.com/1920x900/8f82bc)">
-                        <div class="inner">
-                            <p class="tit">꾸준한 역량개발로<br>프론트 엔드 개발자 성장 중</p>
-                        </div>
+<!-- #container -->
+<div id="container" role="main">
+    <!-- 메인비주얼 -->
+    <section id="visual">
+        <div class="inner">
+            <h3 class="tit_section">안녕하세요,<br><strong>UI 마크업 개발자 장지훈</strong> 입니다.</h3>
+            <button class="videoLayer" data-src="https://www.youtube.com/embed/sqgxcCjD04s" data-title="영상제목, 영상제목, 영상제목, 영상제목, 영상제목, 영상제목, 영상제목, 영상제목, 영상제목, 영상제목, 영상제목, 영상제목, 영상제목, " data-type="youtube" type="button" title="영상재생">1분 자기소개 영상</button>
+        </div> 
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" :style="'background-image:url('+imgPath+'visual_bg1.jpg)'">
+                    <div class="inner">
+                        <p class="tit">다양한 디바이스 크로스 부라우징 기술력을 확보한<br>UI개발을 지원하고있습니다.</p>
                     </div>
                 </div>
-                <!-- Add Navigation -->
-                <div class="swiper-button-area">
-                    <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
-                    <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
-                    <div class="progress_bar"></div>
+                <div class="swiper-slide" style="background-image:url(https://via.placeholder.com/1920x900/89c997)">
+                    <div class="inner">
+                        <p class="tit">생산성으로<br>가장 효율적인 방법으로</p>
+                    </div>
                 </div>
-                <!-- Add Pagination -->
-                <div class="swiper-pagination"></div>
-            </div>
-        </section>
-        <!-- //메인비주얼 -->
-
-        <!-- 약력 -->
-        <section id="profile">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <p class="tit">올해로 만 38세가 되었습니다.</p>
-                        <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
-                        <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
+                <div class="swiper-slide" style="background-image:url(https://via.placeholder.com/1920x900/7ecef4)">
+                    <div class="inner">
+                        <p class="tit">광고 디자인~개발까지<br>다양한 역량 보유</p>
                     </div>
-                    <div class="swiper-slide">
-                        <p class="tit">올해로 만 38세가 되었습니다.</p>
-                        <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
-                        <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
+                </div>
+                <div class="swiper-slide" style="background-image:url(https://via.placeholder.com/1920x900/f29b76)">
+                    <div class="inner">
+                        <p class="tit">SEO, 시멘트 마크업으로<br>트래픽 향상에 최적</p>
                     </div>
-                    <div class="swiper-slide">
-                        <p class="tit">올해로 만 38세가 되었습니다.</p>
-                        <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
-                        <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
-                    </div>
-                    <div class="swiper-slide">
-                        <p class="tit">올해로 만 38세가 되었습니다.</p>
-                        <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
-                        <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
-                    </div>
-                    <div class="swiper-slide">
-                        <p class="tit">올해로 만 38세가 되었습니다.</p>
-                        <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
-                        <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
+                </div>
+                <div class="swiper-slide" style="background-image:url(https://via.placeholder.com/1920x900/8f82bc)">
+                    <div class="inner">
+                        <p class="tit">꾸준한 역량개발로<br>프론트 엔드 개발자 성장 중</p>
                     </div>
                 </div>
             </div>
             <!-- Add Navigation -->
-            <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
-            <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+            <div class="swiper-button-area">
+                <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+                <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+                <div class="progress_bar"></div>
+            </div>
             <!-- Add Pagination -->
             <div class="swiper-pagination"></div>
-        </section>
-        <!-- //약력 -->
+        </div>
+    </section>
+    <!-- //메인비주얼 -->
 
-        <!-- 프로젝트 -->
-        <section id="project">
-            <div class="inner">
-                <p class="tit_section">광고디자인, UI/UX, 퍼블리싱까지<br><strong>다양한 프로젝트 스토리</strong></p>
-                <div class="list_tag">
-                    <button class="btn_tag on" type="button">기업홈페이지</button>
-                    <button class="btn_tag" type="button">사업소개</button>
-                    <button class="btn_tag" type="button">광고디자인</button>
-                    <button class="btn_tag" type="button">모션그래픽</button>
-                    <button class="btn_tag" type="button">패키지디자인</button>
-                    <button class="btn_tag" type="button">웹디자인</button>
-                    <button class="btn_tag" type="button">적응형</button>
-                    <button class="btn_tag" type="button">반응형</button>
-                    <button class="btn_tag" type="button">PC</button>
-                    <button class="btn_tag" type="button">MOBILe</button>
+    <!-- 약력 -->
+    <section id="profile">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <p class="tit">올해로 만 38세가 되었습니다.</p>
+                    <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
+                    <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
                 </div>
-                <ul class="list_project">
-                    <li class="item_project">
-                        <div class="box_thumb">
-                            <img src="https://via.placeholder.com/416x240/" alt="">
-                        </div>
-                        <p class="tit">매경경영지원본부</p>
-                        <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
-                        <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>                        
-                    </li>
-                    <li class="item_project">
-                        <div class="box_thumb">
-                            <img src="https://via.placeholder.com/416x240/" alt="">
-                        </div>
-                        <p class="tit">매경경영지원본부</p>
-                        <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
-                        <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
-                    </li>
-                    <li class="item_project">
-                        <div class="box_thumb">
-                            <img src="https://via.placeholder.com/416x240/" alt="">
-                        </div>
-                        <p class="tit">매경경영지원본부</p>
-                        <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
-                        <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
-                    </li>
-                    <li class="item_project">
-                        <div class="box_thumb">
-                            <img src="https://via.placeholder.com/416x240/" alt="">
-                        </div>
-                        <p class="tit">매경경영지원본부</p>
-                        <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
-                        <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
-                    </li>
-                    <li class="item_project">
-                        <div class="box_thumb">
-                            <img src="https://via.placeholder.com/416x240/" alt="">
-                        </div>
-                        <p class="tit">매경경영지원본부</p>
-                        <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
-                        <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
-                    </li>
-                    <li class="item_project">
-                        <div class="box_thumb">
-                            <img src="https://via.placeholder.com/416x240/" alt="">
-                        </div>
-                        <p class="tit">매경경영지원본부</p>
-                        <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
-                        <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
-                    </li>
-                </ul>
-                <div class="btn_area">
-                    <button class="btn_more" type="button"><img class="ico" src="" data-images-path="/images/ico/arr_bottom01.svg"><span>더보기</span></button>
+                <div class="swiper-slide">
+                    <p class="tit">올해로 만 38세가 되었습니다.</p>
+                    <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
+                    <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
+                </div>
+                <div class="swiper-slide">
+                    <p class="tit">올해로 만 38세가 되었습니다.</p>
+                    <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
+                    <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
+                </div>
+                <div class="swiper-slide">
+                    <p class="tit">올해로 만 38세가 되었습니다.</p>
+                    <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
+                    <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
+                </div>
+                <div class="swiper-slide">
+                    <p class="tit">올해로 만 38세가 되었습니다.</p>
+                    <p class="num" data-animateNumber="1983.02.01">1983.02.01</p>
+                    <p class="desc">1남 2녀 막내로 충정북도 충주에서 태어났으며,<br> 크리스천인 부모님 영향으로 이타심의 중요성 배우며 성장했습니다.</p>
                 </div>
             </div>
-        </section>
-        <!-- //프로젝트 -->
-    </div>
+        </div>
+        <!-- Add Navigation -->
+        <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>
+        <div class="swiper-button-next swiper-button-white" slot="button-next"></div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+    </section>
+    <!-- //약력 -->
+
+    <!-- 프로젝트 -->
+    <section id="project">
+        <div class="inner">
+            <p class="tit_section">광고디자인, UI/UX, 퍼블리싱까지<br><strong>다양한 프로젝트 스토리</strong></p>
+            <div class="list_tag">
+                <button class="btn_tag on" type="button">기업홈페이지</button>
+                <button class="btn_tag" type="button">사업소개</button>
+                <button class="btn_tag" type="button">광고디자인</button>
+                <button class="btn_tag" type="button">모션그래픽</button>
+                <button class="btn_tag" type="button">패키지디자인</button>
+                <button class="btn_tag" type="button">웹디자인</button>
+                <button class="btn_tag" type="button">적응형</button>
+                <button class="btn_tag" type="button">반응형</button>
+                <button class="btn_tag" type="button">PC</button>
+                <button class="btn_tag" type="button">MOBILe</button>
+            </div>
+            <ul class="list_project">
+                <li class="item_project">
+                    <div class="box_thumb">
+                        <img src="https://via.placeholder.com/416x240/" alt="">
+                    </div>
+                    <p class="tit">매경경영지원본부</p>
+                    <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
+                    <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>                        
+                </li>
+                <li class="item_project">
+                    <div class="box_thumb">
+                        <img src="https://via.placeholder.com/416x240/" alt="">
+                    </div>
+                    <p class="tit">매경경영지원본부</p>
+                    <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
+                    <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
+                </li>
+                <li class="item_project">
+                    <div class="box_thumb">
+                        <img src="https://via.placeholder.com/416x240/" alt="">
+                    </div>
+                    <p class="tit">매경경영지원본부</p>
+                    <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
+                    <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
+                </li>
+                <li class="item_project">
+                    <div class="box_thumb">
+                        <img src="https://via.placeholder.com/416x240/" alt="">
+                    </div>
+                    <p class="tit">매경경영지원본부</p>
+                    <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
+                    <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
+                </li>
+                <li class="item_project">
+                    <div class="box_thumb">
+                        <img src="https://via.placeholder.com/416x240/" alt="">
+                    </div>
+                    <p class="tit">매경경영지원본부</p>
+                    <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
+                    <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
+                </li>
+                <li class="item_project">
+                    <div class="box_thumb">
+                        <img src="https://via.placeholder.com/416x240/" alt="">
+                    </div>
+                    <p class="tit">매경경영지원본부</p>
+                    <p class="desc">'지속가능한 경영'을 위한 리스크관리부터 컨설팅 후 사후관리까지 중소기업 위기극복 파트너</p>
+                    <p class="tag"><span>기업홈페이지</span><span>사업소개</span><span>적응형</span></p>
+                </li>
+            </ul>
+            <div class="btn_area">
+                <button class="btn_more" type="button"><img class="ico" src="" data-images-path="/images/ico/arr_bottom01.svg"><span>더보기</span></button>
+            </div>
+        </div>
+    </section>
+    <!-- //프로젝트 -->
+</div>
+<!-- //#container -->
 </template>
 
 
