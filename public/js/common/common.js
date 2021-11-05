d0354dc6a9e5dd4738dcd3f4703d11f5d173e63f
me2designer.com <me2designer@naver.com>
2021-11-05 17:59:10 +0900

    애널리틱스 연결

--- public/js/common/common.js
+++ public/js/common/common.js
@@ -2,9 +2,9 @@ $(function(){/*
 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 */(function(){
 
-    
 
-    /* wrapper is ready */ 
+
+    /* wrapper is ready */
     var $wrap = $('#wrap');
 
     if ($wrap.css('opacity') == '0' || $wrap.css('visibility') == 'hidden') {
@@ -14,16 +14,8 @@ $(function(){/*
         });
     }
 
-    var $lodingWrap = $('#lodingWrap');
-    $lodingWrap.remove();
-
-
-
-})();/*
-■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-*/(function(){
-
-
+    // Loding
+    $('#lodingWrap').remove();
 
 
 
@@ -54,6 +46,75 @@ $(function(){/*
 */(function(){
 
 
+
+    /* HEADER - is-active */
+    var $header = $('#header');
+    var status;
+
+    $(window).on('scroll', function () {
+        var windowT = $(this).scrollTop();
+        if (windowT >= 50){
+            if (!status) $header.addClass('is-active');
+            status = true;
+        } else {
+            if (status) $header.removeClass('is-active');
+            status = false;
+        }
+    });
+
+
+
+})();/*
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+*/(function(){
+
+
+
+    /* HEADER - 현재 section 위치표시 및 바로가기 */
+    var $header = $('#header');
+
+    //현재위치표시
+    var $section = $('#container > section');
+    var $btn = $header.find('.btn_gnb');
+
+    $section.each(function() {
+        var $this = $(this);
+
+        scrollAction({
+            target: $this,
+            top: 20,
+            scrollDownAction : function(){
+                // 스크롤 DOWN 액션
+                var id = '#'+$this.attr('id');
+
+                $btn.removeClass('on');
+                $btn.siblings('[data-target="'+id+'"]').addClass('on');
+
+            },
+            scrollUpAction : function(){
+                // 스크롤 UP 액션
+                var id = '#'+$this.attr('id');
+                var $prev = $btn.siblings('[data-target="'+id+'"]').prev();
+                var prevId = $prev.attr('data-target');
+
+                $btn.removeClass('on');
+                $btn.siblings('[data-target="'+prevId+'"]').addClass('on');
+            }
+        });
+    });
+
+    //.moveTo()
+    var headerH = $header.outerHeight();
+
+    $btn.on('click', function(){
+        var target = $(this).attr('data-target')||$(this).attr('data-anchor');
+        var top = $(this).index() == 0 ? 0 : headerH;                
+
+        moveTo({
+            top : top,
+            target : target,
+        });        
+    });
 
 
 
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,15 +2,15 @@
     <div id="header">
         <div class="inner">
             <h1 class="logo">
-                <a href="/" title="메인으로 이동하기"><strong>ME2DESIGNER</strong>포트폴리오</a>
+                <a class="link_home" href="/" title="메인으로 이동하기"><strong>ME2DESIGNER</strong>포트폴리오</a>
             </h1>
             <nav class="gnb">
-                <button class="btn_gnb" type="button">1분 자기소개 영상</button>
-                <button class="btn_gnb" type="button">성장과정</button>
-                <button class="btn_gnb" type="button">프로젝트 소개</button>
-                <button class="btn_gnb" type="button">경력개발</button>
-                <button class="btn_gnb" type="button">근무이력</button>
-                <button class="btn_gnb" type="button">코딩블로그</button>
+                <button class="btn_gnb" data-target="#visual" type="button">1분 자기소개 영상</button>
+                <button class="btn_gnb" data-target="#profile" type="button">성장과정</button>
+                <button class="btn_gnb" data-target="#project" type="button">프로젝트 소개</button>
+                <button class="btn_gnb" data-target="#study" type="button">경력개발</button>
+                <button class="btn_gnb" data-target="#history" type="button">근무이력</button>
+                <button class="btn_gnb" data-target="#blog" type="button">코딩블로그</button>
             </nav>
         </div>
     </div>
