9b63b2748da08252b8c86ea53d50999e8187d59d
me2designer.com <me2designer@naver.com>
2021-11-09 16:26:41 +0900

    api 수정

--- docs/js/common/api.js
+++ docs/js/common/api.js
@@ -4,20 +4,23 @@
 
 
     function getTistory(count, pageNum) {
+        var list;
         var pars = {
             'accessToken' : '014f0adecdf2a12798c783abbc7a0498_3ea393c3f6e235ff61d1f6e3557bbdb9',
             'outputType' : 'json', //xml 또는 json
             'blogName' : 'https://me2designer.tistory.com'
         }
-        $.ajax({
-            type:'GET',
-            url: 'https://www.tistory.com/apis/post/list?access_token='+pars.accessToken+'&output='+pars.outputType+'&blogName='+pars.blogName+'&count='+count+'&page='+pageNum+'&visibility=3'
-        }).done(function(data) {
-            console.log(data)
+        $.ajax({            
+            url: 'https://www.tistory.com/apis/post/list?access_token='+pars.accessToken+'&output='+pars.outputType+'&blogName='+pars.blogName+'&count='+count+'&page='+pageNum+'&visibility=0',
+            async : false,
+            success : function(data){
+                list = data.tistory.item;
+            }
         });
+        return list;
     };
     
-    getTistory(4,1)
+    console.log(getTistory(4,1));
 
 /*
 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
